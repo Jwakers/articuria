bc73d4fa8b75eee945e12ff1bd994d09998c1a67
Jack Wakeham <jackwakeham82@gmail.com>
2025-08-09 07:48:09 +0100

    feat: add loading skeleton for contact page

---
+++ app/(authed)/dashboard/contact/loading.tsx
@@ -0,0 +1,36 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  return (
+    <div className="grid max-w-xl gap-4 md:grid-cols-2">
+      {/* Name field skeleton */}
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-12" />
+        <Skeleton className="h-10 w-full" />
+      </div>
+
+      {/* Email field skeleton */}
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-12" />
+        <Skeleton className="h-10 w-full" />
+      </div>
+
+      {/* Reason dropdown skeleton */}
+      <div className="space-y-2 md:col-span-2">
+        <Skeleton className="h-4 w-32" />
+        <Skeleton className="h-10 w-full" />
+      </div>
+
+      {/* Message textarea skeleton */}
+      <div className="space-y-2 md:col-span-2">
+        <Skeleton className="h-4 w-20" />
+        <Skeleton className="h-[150px] w-full" />
+      </div>
+
+      {/* Submit button skeleton */}
+      <div className="md:col-span-2">
+        <Skeleton className="h-10 w-full" />
+      </div>
+    </div>
+  );
+}
